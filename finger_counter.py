f71249277369395a481f58921c6328a9d9625909
saadaoui-salah <salahsaadaoui8@gmail.com>
2021-04-09 17:31:14 +0100

    add fps

--- finger_counter.py
+++ finger_counter.py
@@ -4,8 +4,8 @@ import os
 import HandTrackingModule
 
 capture = cv2.VideoCapture(0) 
-capture.set(3,900)
-capture.set(4,800)
+capture.set(3,480)
+capture.set(4,700)
 
 folder = "data"
 dirs = os.listdir(folder)
@@ -21,21 +21,23 @@ while True:
 
     success, img = capture.read()
     if success:
+        current_time = time.time()
         h,w,c = images[0].shape
         img[0:h,0:w] = images[0]
         
-        
-        current_time = time.time()
-        fps = int(1/(current_time-previouse_time))
-        previouse_time = current_time
-        cv2.putText(
-            img,
-            f'FPS: {str(fps)}',
-            (0,0),
-            cv2.FONT_HERSHEY_COMPLEX,
-            3,
-            (0,0,0),
-            3)
+        try:
+            fps = int(1/(current_time-previouse_time))
+            previouse_time = current_time
+            cv2.putText(
+                img,
+                f'FPS: {str(fps)}',
+                (0,400),# location
+                cv2.FONT_HERSHEY_COMPLEX,# text font
+                1,# scale
+                (100,100,100),# color
+                3)# thiknes
+        except ZeroDivisionError:
+            pass       
         cv2.imshow("Image",img)
         cv2.waitKey(1)
 
